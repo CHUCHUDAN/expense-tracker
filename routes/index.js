bc5259a33146aa121b61431da32c733728d3fc03
CHUCHUDAN <daniel870217@gmail.com>
2023-04-04 11:23:14 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const cruds = require('./modules/cruds')
+const users = require('./modules/users')
 
-
+router.use('/users', users)
 router.use('/cruds', cruds)
 router.use('/', home)
 
