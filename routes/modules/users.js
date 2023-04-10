9be8ad53bf885214b5c7b57796d909cfcd646ddf
CHUCHUDAN <daniel870217@gmail.com>
2023-04-10 14:43:21 +0800

    feat: add bcyrptjs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/users')
 const passport = require('passport')
-
+const bcrypt = require('bcryptjs')
 //login頁面
 router.get('/login', (req, res) => {
   res.render('login')
@@ -27,29 +27,32 @@ router.get('/register', (req, res) => {
 })
 
 //register功能
-router.post('/register', (req, res) => {
-  const { name, email, password, confirmpassword } = req.body
-  const errors = []
-  if (!email || !password || !confirmpassword) {
-    errors.push({message: '帳號及密碼都是必填!'})
-  } 
-  if (password !== confirmpassword) {
-    errors.push({ message: '密碼及確認密碼不相符!' })
-  } 
-  if (errors.length) {
-    return res.render('register', { name, email, password, confirmpassword, errors })
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, confirmpassword } = req.body
+    const errors = []
+    if (!email || !password || !confirmpassword) {
+      errors.push({ message: '帳號及密碼都是必填!' })
+    }
+    if (password !== confirmpassword) {
+      errors.push({ message: '密碼及確認密碼不相符!' })
+    }
+    if (errors.length) {
+      return res.render('register', { name, email, password, confirmpassword, errors })
+    }
+    const userEmail = await User.findOne({email})
+    if (userEmail) {
+      errors.push({ message: '此信箱已註冊過' })
+      return res.render('register', { name, email, password, confirmpassword, errors })
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    await User.create({name, email, password: hash})
+    req.flash('success_msg', '註冊成功')
+    res.redirect('/users/login')
+  } catch (error) {
+    console.error(error)
   }
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        errors.push({message: '此信箱已註冊過'})
-        return res.render('register', { name, email, password, confirmpassword, errors })
-      }
-      User.create({ name, email, password })
-      .then(res.redirect('/'))
-      .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
 })
 
 module.exports = router
