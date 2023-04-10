065d3fa95cf02cfe0030ddc16df4a132f8d5476a
CHUCHUDAN <daniel870217@gmail.com>
2023-04-10 15:01:53 +0800

    feat: add bcyrpt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/users')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
   //初始化passport
@@ -8,28 +9,32 @@ module.exports = app => {
   app.use(passport.session())
 
   //設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not register!' })
-        }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
-        }
-        return done(null, user)
-      })
-      .catch(err => done(err, false))
+  passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({email})
+      if (!user) {
+        return done(null, false, { message: 'That email is not register!' })
+      }
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return done(null, false, { message: 'Email or password  incorrect' })
+      }
+      return done(null, user)
+    }catch (error) {
+      return done(err, false)
+    }
   }))
 
   //設定序列化及反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
-  passport.deserializeUser((id, done) => {
-    User.findById(id)
-      .lean()
-      .then(user => done(null, user))
-      .catch(err => done(err, null))
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id).lean()
+      return done(null, user)
+    }catch (error) {
+      return done(err, null)
+    }
   })
 }
