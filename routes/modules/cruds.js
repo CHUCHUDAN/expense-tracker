bb96be27e306ffab6fcf5e8fd419ec9cd8986da6
CHUCHUDAN <daniel870217@gmail.com>
2023-04-25 16:34:51 +0800

    updated

--- routes/modules/cruds.js
+++ routes/modules/cruds.js
@@ -18,10 +18,15 @@ router.post('/new', async (req, res) => {
   try {
     const userId = req.user._id
     const { name, date, amount, categoryId } = req.body
+    const c = await CG.find().lean()
+    if (!name || !date || !amount || !categoryId) {
+      req.flash('warning_msg', '所有欄位都是必填')
+      return res.render('new', { name, date, amount, categoryId, category: c })
+    }
     let icon = ''
     const category = await CG.find({ _id: categoryId }).lean()
     const [cate] = category
-    await Record.create({ name, date, amount, icon: cate.icon, categoryId, userId,  })
+    await Record.create({ name, date, amount, icon: cate.icon, categoryId, userId, })
     res.redirect('/')
   } catch (error) {
     console.error(error)
@@ -51,17 +56,27 @@ router.put('/:id', async (req, res) => {
   try {
     const userId = req.user._id
     const _id = req.params.id
+    const rec = req.body
+    rec._id = _id
     const { name, date, categoryId, amount } = req.body
-    const category = await CG.find({ _id: categoryId }).lean()
-    const [cate] = category
-    const record = await Record.findOne({ _id, userId })
-    record.name = name
-    record.date = date
-    record.categoryId = categoryId
-    record.amount = amount
-    record.icon = cate.icon
-    await record.save()
-    res.redirect('/')
+    const category = await CG.find().lean()
+    const selectedData = category.filter(cate => cate._id.toString() === categoryId)
+    const remindData = category.filter(cate => cate._id.toString() !== categoryId)
+    if (!name || !date || !amount || !categoryId) {
+      req.flash('warning_msg', '所有欄位都是必填')
+      return res.render('edit', { selectedData, remindData, record: rec })
+    }else {
+      const category = await CG.find({ _id: categoryId }).lean()
+      const [cate] = category
+      const record = await Record.findOne({ _id, userId })
+      record.name = name
+      record.date = date
+      record.categoryId = categoryId
+      record.amount = amount
+      record.icon = cate.icon
+      await record.save()
+      res.redirect('/')
+    }
   } catch (error) {
     console.error(error)
   }
@@ -75,7 +90,7 @@ router.delete('/:id', async (req, res) => {
     const record = await Record.findOne({ _id, userId })
     record.remove()
     res.redirect('/')
-  }catch (error) {
+  } catch (error) {
     console.error(error)
   }
 })
